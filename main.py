53c6d4f2756bf4cd76ee0ab7fcf3724eaf0cea81
MusaabShalaldeh <musaabsh65@yahoo.com>
2021-11-07 15:07:31 +0200

    feat: implemented application start and the ability to select a game.

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-def say_hi():
-    print("Hi!")
-    return "Hi"
-    
+"""
+Main Game Module
+"""
+from colorama import Fore, Style
+from PyInquirer import prompt
+import pyfiglet
+
+
+def start_application():
+    """
+    Application Start Method
+    """
+    start_message = Fore.CYAN + Style.BRIGHT + pyfiglet.figlet_format("PyArcade Games")
+    print(start_message)
+    question = [
+        {
+            "type": "list",
+            "name": "game",
+            "message": "Choose A Game To Play.",
+            "choices": [
+                "Hang Man",
+                "Quiz Game",
+                "Snake",
+                "Pylearn",
+                "View Scores",
+                "Quit"
+            ],
+        }
+    ]
+
+    answer = prompt(question).get("game")
+
+
+if __name__ == "__main__":
+    start_application()
