f1df4d804ab50f6e00f38b185cd902e3847b8f5c
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2021-11-07 15:24:28 +0200

    docs : docs strings added for main and display method

--- pyarcade_games/quiz_game.py
+++ pyarcade_games/quiz_game.py
@@ -3,18 +3,35 @@ import readchar
 import sys
 
 def display(msg,style):
-	f = Figlet(font=style)
-	result=f.renderText(msg)
-	print(result)
-	return result
+    """
+    Display function for ASCII art text generation 
 
-def main(): 
-	display("     Covid-19 Quiz Game","small")	
-	print('Enter s to start the game, q to exit : ')
-	char=readchar.readchar()
-	if char== 's':
-		# ------------------------- Play ------------------------------
-		display("     Quiz Game","small")
-		print("Welcome to the quiz game, we have 10 questions to answer and you will get your score at the end, Good Luck!")
-	else:
-		main()
+    Arg: 
+        msg: string argument for the messege to be printed
+        style: special argument to select the style of messge to be printed
+    Return:
+        result : a messege in ASCII art style
+    """
+    f = Figlet(font=style)
+    result=f.renderText(msg)
+    print(result)
+    return result
+
+def main():
+    """
+    Main function for Runinng the game
+
+    Arg: 
+        No Argument
+    Return:
+        No return Value
+    """ 
+    display("     Covid-19 Quiz Game","small")	
+    print('Enter s to start the game, q to exit : ')
+    char=readchar.readchar()
+    if char== 's':
+        # ------------------------- Play ------------------------------
+        display("     Quiz Game","small")
+        print("Welcome to the quiz game, we have 10 questions to answer and you will get your score at the end, Good Luck!")
+    else:
+        main()
