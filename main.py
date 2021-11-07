1e95a212ed1ca90d4bd9bfee3e472f261291ed4e
MusaabShalaldeh <musaabsh65@yahoo.com>
2021-11-07 15:49:08 +0200

    feat: implemented the ability to load the chosen game.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Main Game Module
 from colorama import Fore, Style
 from PyInquirer import prompt
 import pyfiglet
+import sys
 
 
 def start_application():
@@ -29,6 +30,18 @@ def start_application():
     ]
 
     answer = prompt(question).get("game")
+    if answer == "Hang Man":
+        from pyarcade_games.hangman import main
+        main()
+    elif answer == "Quiz Game":
+        from pyarcade_games.quiz_game import main
+        main()
+    elif answer == "Snake":
+        from pyarcade_games.snake_game import main
+        main()
+    elif answer == "Pylearn":
+        from pyarcade_games.pylearn import main
+        main()
 
 
 if __name__ == "__main__":
