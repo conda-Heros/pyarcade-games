51a00a78f232c5ea7b566addcfadd9c6dadd5700
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2021-11-11 14:26:34 +0200

    test : hangman game review with tests added

--- tests/test_hangman.py
+++ tests/test_hangman.py
@@ -1,7 +1,8 @@
-from pyarcade_games.hangman import check_if_char_in_word, underscorify_word
-
-
-
+from pyarcade_games.hangman import *
+import builtins
+import difflib
+import pytest
+import sys
 
 def test_check_if_char_in_word():
     # Arrange
@@ -43,8 +44,6 @@ def test_underscorify_word_with_num():
     # Expected
     assert actual == expected
 
-
-
 def test_underscorify_word_with_num():
     # Arrange
     expected = None
@@ -53,3 +52,70 @@ def test_underscorify_word_with_num():
     # Expected
     print(actual)
     assert actual == expected
+
+@pytest.mark.skip("todo")
+def test_start_hangman_game_method():
+    diffs = diff(start_hangman_game(4,"abcd"), path="assets/scenario/senario3.txt")
+    assert not diffs, diffs
+
+def diff(play, path="", sample=""):
+    text = ""
+    expected_lines = _parse_expected_lines(path, sample)
+    responses = _extract_responses(expected_lines)
+    def mock_print(*args):
+
+        nonlocal text
+
+        text += "".join(args) + "\n"
+
+    def mock_input(*args):
+
+        nonlocal text
+
+        if not responses:
+            sys.exit(1)
+
+        response = responses.pop(0)
+
+        text += "".join(args) + response + "\n"
+
+        return response
+    
+    real_print = builtins.print
+    real_input = builtins.input
+
+    builtins.print = mock_print
+    builtins.input = mock_input
+
+    try:
+        start_hangman_game(6,"abcd",1)
+    except SystemExit:
+        real_print("No problem. System exits are allowed in this app.")
+
+    builtins.print = real_print
+    builtins.input = real_input
+
+    return _find_differences(text, expected_lines)
+def _parse_expected_lines(path, sample):
+    if path:
+        with open(path) as f:
+            expected_lines = f.read().splitlines()
+    else:
+        expected_lines = sample.splitlines()
+
+    return expected_lines
+def _extract_responses(lines):
+    responses = []
+    for line in lines:
+        if line.startswith(">"):
+            response = line.replace("> ", "").strip()
+            responses.append(response)
+    print(responses)
+    return responses
+def _find_differences(text, expected_lines):
+
+    actual_lines = text.splitlines()
+
+    diffed = difflib.unified_diff(actual_lines, expected_lines, lineterm="")
+
+    return "\n".join(diffed)
